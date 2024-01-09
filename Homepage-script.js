82efc75d497d4c41be427b12fcd6622a65bb558b
Diep Nguyen <131941767+Diep-Nguyen-Tara@users.noreply.github.com>
2024-01-08 21:36:40 -0500

    Create Homepage-script.js

---
+++ Homepage-script.js
@@ -0,0 +1,9 @@
+<!-- Add this script tag in the head of your HTML pages -->
+<script src="script.js"></script>
+
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome to My Homepage!');
+});
